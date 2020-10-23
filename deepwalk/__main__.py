3122cc0b2ea6c48962101e66a39ce9d6365ba9dd
yycg <yingyingchongge@gmail.com>
2020-10-23 11:31:37 +0800

    import error

--- deepwalk/__main__.py
+++ deepwalk/__main__.py
@@ -10,10 +10,11 @@ from collections import Counter
 from concurrent.futures import ProcessPoolExecutor
 import logging
 
-from . import graph
-from . import walks as serialized_walks
+# https://github.com/phanein/deepwalk/issues/29
+from deepwalk import graph
+from deepwalk import walks as serialized_walks
 from gensim.models import Word2Vec
-from .skipgram import Skipgram
+from deepwalk.skipgram import Skipgram
 
 from six import text_type as unicode
 from six import iteritems
