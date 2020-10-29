3f69a3edb73dc83dbb0d46667157259c8f663f69
yycg <yingyingchongge@gmail.com>
2020-10-29 16:06:32 +0800

    build graph

--- experiment1/src/douban/build_graph.py
+++ experiment1/src/douban/build_graph.py
@@ -3,11 +3,11 @@ import itertools
 
 data_path = "../../data/douban"
 
-if __name__ == "__main__":
+def build_graph():
     with open(os.path.join(data_path, "user_item_list.txt"), "r") as reader, \
             open(os.path.join(data_path, "user_item_edge.txt"), "w") as writer:
         for line in reader:
-            items = line.split(" ")
+            items = line.strip().split(" ")
             edges = itertools.permutations(items, 2)
             for edge in edges:
                 writer.write(" ".join(edge) + "\n")
@@ -15,7 +15,12 @@ if __name__ == "__main__":
     with open(os.path.join(data_path, "category_item_list.txt"), "r") as reader, \
             open(os.path.join(data_path, "category_item_edge.txt"), "w") as writer:
         for line in reader:
-            items = line.split(" ")
+            items = line.strip().split(" ")
             category = items[0]
             items = items[1:]
+            edges = itertools.permutations(items, 2)
+            for edge in edges:
+                writer.write(category + " " + " ".join(edge) + "\n")
 
+if __name__ == "__main__":
+    build_graph()
