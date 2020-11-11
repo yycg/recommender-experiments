5645bcb5928d3f1ad4da96cad826862ff30825e5
yycg <yingyingchongge@gmail.com>
2020-11-11 17:38:41 +0800

    ccse

---
+++ smore/src/model/CCSE.h
@@ -0,0 +1,40 @@
+#ifndef CCSE_H
+#define CCSE_H
+
+#include "../proNet.h"
+
+/*****
+ * CCSE
+ * **************************************************************/
+
+class CCSE {
+
+    public:
+        
+        CCSE();
+        ~CCSE();
+        
+        proNet pnet;
+
+        // parameters
+        int dim;                // representation dimensions
+        vector< vector<double> > w_vertexU;
+        vector< vector<double> > w_vertexI;
+        vector< vector<double> > w_contextU;
+        vector< vector<double> > w_contextI;
+        vector< vector<double> > w_contextUU;
+        vector< vector<double> > w_contextII;
+
+        // data function
+        void LoadEdgeList(string, bool);
+        void LoadFieldMeta(string);
+        void SaveWeights(string);
+        
+        // model function
+        void Init(int);
+        void Train(int, int, double, int);
+
+};
+
+
+#endif
