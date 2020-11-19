e328b20501b2ef988ae85db43060b5e1e0dea6dd
yycg <yingyingchongge@gmail.com>
2020-11-19 16:57:22 +0800

    tune parameters

---
+++ experiment2/src/douban/parameters_tuning.py
@@ -0,0 +1,28 @@
+from concurrent.futures import ThreadPoolExecutor
+import os
+
+
+def run_ccse(sample_times, walk_steps, alpha, dimensions, lambda1, lambda2, lambda_factor):
+    cmd = "python ccse.py --sample_times {0} --walk_steps {1} --alpha {2} --dimensions {3} --lambda1 {4} " \
+          "--lambda2 {5} --lambda_factor {6}"\
+        .format(sample_times, walk_steps, alpha, dimensions, lambda1, lambda2, lambda_factor)
+    print(cmd)
+    os.system(cmd)
+
+
+def main():
+    # https://www.jianshu.com/p/b9b3d66aa0be
+    executor = ThreadPoolExecutor(max_workers=10)
+    for sample_times in [20, 40, 60, 80, 100]:
+        for walk_steps in [40, 60, 80]:
+            for alpha in [0.0025, 0.01, 0.025, 0.1]:
+                for dimensions in [64, 128, 256, 512]:
+                    for lambda1 in [0.0025, 0.01, 0.025, 0.1, 0.25]:
+                        for lambda2 in [0.0025, 0.01, 0.025, 0.1, 0.25]:
+                            for lambda_factor in [0.0025, 0.01, 0.025, 0.1, 0.25]:
+                                executor.submit(run_ccse, (sample_times, walk_steps, alpha, dimensions, lambda1,
+                                                           lambda2, lambda_factor))
+
+
+if __name__ == '__main__':
+    main()
