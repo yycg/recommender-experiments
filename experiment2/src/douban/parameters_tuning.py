233eff5ef9ca04734ef2a2ebd98221f6c36e613d
yycg <yingyingchongge@gmail.com>
2020-11-19 17:01:04 +0800

    tune parameters

--- experiment2/src/douban/parameters_tuning.py
+++ experiment2/src/douban/parameters_tuning.py
@@ -1,4 +1,4 @@
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
 
 
@@ -8,10 +8,12 @@ def run_ccse(sample_times, walk_steps, alpha, dimensions, lambda1, lambda2, lamb
         .format(sample_times, walk_steps, alpha, dimensions, lambda1, lambda2, lambda_factor)
     print(cmd)
     os.system(cmd)
+    return cmd
 
 
 def main():
     # https://www.jianshu.com/p/b9b3d66aa0be
+    all_task = []
     executor = ThreadPoolExecutor(max_workers=10)
     for sample_times in [20, 40, 60, 80, 100]:
         for walk_steps in [40, 60, 80]:
@@ -20,9 +22,11 @@ def main():
                     for lambda1 in [0.0025, 0.01, 0.025, 0.1, 0.25]:
                         for lambda2 in [0.0025, 0.01, 0.025, 0.1, 0.25]:
                             for lambda_factor in [0.0025, 0.01, 0.025, 0.1, 0.25]:
-                                executor.submit(run_ccse, (sample_times, walk_steps, alpha, dimensions, lambda1,
-                                                           lambda2, lambda_factor))
-
+                                all_task.append(executor.submit(run_ccse, (sample_times, walk_steps, alpha, dimensions,
+                                                                           lambda1, lambda2, lambda_factor)))
+    for future in as_completed(all_task):
+        data = future.result()
+        print("{} succeed".format(data))
 
 if __name__ == '__main__':
     main()
