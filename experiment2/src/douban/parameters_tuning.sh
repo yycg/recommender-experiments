43a49e5b67193858d6d698fb0eeaa10ff345211b
yycg <yingyingchongge@gmail.com>
2020-11-19 16:23:34 +0800

    tune parameters

--- experiment2/src/douban/parameters_tuning.sh
+++ experiment2/src/douban/parameters_tuning.sh
@@ -5,9 +5,9 @@ for sample_times in 20 40 60 80 100
         do for lambda1 in "0.0025" "0.01" "0.025" "0.1" "0.25"
           do for lambda2 in "0.0025" "0.01" "0.025" "0.1" "0.25"
             do for lambda_factor in "0.0025" "0.01" "0.025" "0.1" "0.25"
-              # do echo "./ccse.py --sample_times $sample_times --walk_steps $walk_steps --alpha $alpha --dimensions $dimensions --lambda1 $lambda1 --lambda2 $lambda2 --lambda_factor $lambda_factor"
+              # do echo "python ccse.py --sample_times $sample_times --walk_steps $walk_steps --alpha $alpha --dimensions $dimensions --lambda1 $lambda1 --lambda2 $lambda2 --lambda_factor $lambda_factor"
               do {
-                ./ccse.py --sample_times $sample_times --walk_steps $walk_steps --alpha $alpha --dimensions $dimensions --lambda1 $lambda1 --lambda2 $lambda2 --lambda_factor $lambda_factor
+                python ccse.py --sample_times $sample_times --walk_steps $walk_steps --alpha $alpha --dimensions $dimensions --lambda1 $lambda1 --lambda2 $lambda2 --lambda_factor $lambda_factor
               }&
             done
           done
