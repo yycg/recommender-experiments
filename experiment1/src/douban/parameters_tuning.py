dc9210343c7a4b93a322f55821fdbb995199a19b
yycg <yingyingchongge@gmail.com>
2020-12-03 17:11:28 +0800

    tune parameters

--- experiment1/src/douban/parameters_tuning.py
+++ experiment1/src/douban/parameters_tuning.py
@@ -15,11 +15,16 @@ def main():
     # https://www.jianshu.com/p/b9b3d66aa0be
     all_task = []
     executor = ThreadPoolExecutor(max_workers=20)
-    for number_walks in [10, 40, 80]:  # walk_times
-        for walk_length in [40, 60, 80]:  # walk_steps
-            for representation_size in [64, 128, 256, 512]:  # dimensions
-                for window_size in [2, 3, 4, 5, 6, 8, 10]:  # window_size
-                    for lambda_factor in [0.0025, 0.01, 0.025, 0.1, 0.25, 1, 2.5, 10, 25]:
+    # for number_walks in [10, 40, 80]:  # walk_times
+    #     for walk_length in [40, 60, 80]:  # walk_steps
+    #         for representation_size in [64, 128, 256, 512]:  # dimensions
+    #             for window_size in [2, 3, 4, 5, 6, 8, 10]:  # window_size
+    #                 for lambda_factor in [0.0025, 0.01, 0.025, 0.1, 0.25, 1, 2.5, 10, 25]:
+    for number_walks in [20, 60, 100]:  # walk_times
+        for walk_length in [40]:  # walk_steps
+            for representation_size in [256]:  # dimensions
+                for window_size in [3]:  # window_size
+                    for lambda_factor in [10]:
                         all_task.append(executor.submit(run_category2vec, number_walks, walk_length,
                                                         representation_size, window_size, lambda_factor))
     for future in as_completed(all_task):
